fdecd20f6b0d9ad544b8d874d8d4b5cd08734359
elletech <mygit_yutasugao20171126@yahoo.co.jp>
2021-10-31 02:51:58 +0000

    nodemon

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+app.get("/cources/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
 app.listen(port, () => {
   console.log(`ポート番号${port}で立ち上がりました。`);
 });
